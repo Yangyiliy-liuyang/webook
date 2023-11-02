7f4ee731abc52614b8b4dd1afd4cd8891c0ebca5
yangyiliy <yangyiliy@foxmail.com>
2023-11-02 23:06:19 +0800

    update bcrypt

---
+++ internal/service/testbcrypt.go
@@ -0,0 +1,11 @@
+package service
+
+//func TestBcrypt() {
+//	password := []byte("121js@#hddd")
+//	//加密
+//	hash, err := bcrypt.GenerateFromPassword(password, bcrypt.MinCost)
+//	//解密
+//	fakePassword := "sabdldadsaf"
+//	bcrypt.CompareHashAndPassword(hash, []byte(fakePassword))
+//
+//}
--- internal/service/user.go
+++ internal/service/user.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"golang.org/x/crypto/bcrypt"
 	"webook/internal/domain"
 	"webook/internal/repository"
 )
@@ -14,5 +15,10 @@ func NewUserService(repo *repository.UserRepository) *UserService {
 	return &UserService{repo: repo}
 }
 func (svc *UserService) SingUp(ctx context.Context, u domain.User) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hash)
 	return svc.repo.Create(ctx, u)
 }
