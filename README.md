f12b9e69ffc13d671ff4009af26ae7d3c2670bb4
yangyiliy <yangyiliy@foxmail.com>
2023-11-05 12:47:28 +0800

    update UpdateTime

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # webook
 webook 是一个仿简书的项目
-gin + gorm + cors + regex + bcrypt + sessions
+gin + gorm + cors + regex + bcrypt + sessions + redis + 
 ## 目录结构
 
 ## 启动
+
--- internal/service/middleware/login.go
+++ internal/service/middleware/login.go
@@ -1,9 +1,11 @@
 package middleware
 
 import (
+	"encoding/gob"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 type LoginMiddlewareBuilder struct {
@@ -11,16 +13,33 @@ type LoginMiddlewareBuilder struct {
 
 // CheckLogin 登录校验
 func (m *LoginMiddlewareBuilder) CheckLogin() gin.HandlerFunc {
+	// todo 注册下这个类型
+	gob.Register(time.Now())
 	return func(ctx *gin.Context) {
 		path := ctx.Request.URL.Path
 		if path == "/users/signup" || path == "/users/login" {
 			return
 		}
 		sess := sessions.Default(ctx)
-		if sess.Get("userId") == nil {
+		userId := sess.Get("userId")
+		if userId == nil {
 			//中断
 			ctx.AbortWithStatus(http.StatusServiceUnavailable)
 			return
+		}
+		now := time.Now()
+		const UpdateTimeKey = "update_time"
+		val := sess.Get(UpdateTimeKey)
+		LastUpdateTime, ok := val.(time.Time)
+		if val != nil || !ok || now.Sub(LastUpdateTime) > time.Minute {
+			//第一次进来
+			sess.Set(UpdateTimeKey, now)
+			sess.Set("userId", userId)
+			err := sess.Save()
+			if err != nil {
+				//打印日志
+				println("err")
+			}
 		}
 	}
 }
