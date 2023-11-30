51ee0afdabd0fdef08b71045777932d73bf1a042
yangyiliy <yangyiliy@foxmail.com>
2023-11-30 20:27:21 +0800

    update wechatUrl

--- internal/service/oauth2/wechat/types.go
+++ internal/service/oauth2/wechat/types.go
@@ -1,1 +1,25 @@
 package wechat
+
+import (
+	"context"
+	"fmt"
+	uuid "github.com/lithammer/shortuuid/v4"
+	"net/url"
+)
+
+type Service interface {
+	AuthURL(ctx context.Context) (string, error)
+}
+
+var redirectURL = url.PathEscape(`https://meoying.com/oauth2/wechat/callback`)
+
+type service struct {
+	appId string
+}
+
+func (s *service) AuthURL(ctx context.Context) (string, error) {
+	//https://developers.weixin.qq.com/doc/oplatform/Website_App/WeChat_Login/Wechat_Login.html
+	const authURLPattern = "https://open.weixin.qq.com/connect/qrconnect?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_login&state=%s#wechat_redirect"
+	state := uuid.New()
+	return fmt.Sprintf(authURLPattern, s.appId, redirectURL, state), nil
+}
--- internal/web/wechat.go
+++ internal/web/wechat.go
@@ -1,10 +1,16 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"webook/internal/service/oauth2/wechat"
+)
 
 type OAuth2WechatHandler struct {
+	svc wechat.Service
 }
 
+// RegisterRouters 提供两个接口 接口一，构造跳到微信服务的url 接口二，处理跳转回来的请求
 func (o *OAuth2WechatHandler) RegisterRouters(server *gin.Engine) {
 	g := server.Group("/oauth2/wechat")
 	g.GET("/authurl", o.OAuth2URL)
@@ -12,7 +18,17 @@ func (o *OAuth2WechatHandler) RegisterRouters(server *gin.Engine) {
 }
 
 func (o *OAuth2WechatHandler) OAuth2URL(ctx *gin.Context) {
-
+	url, err := o.svc.AuthURL(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusOK, Result{
+			Msg:  "构造跳转微信登录Url失败",
+			Code: 5,
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, Result{
+		Data: url,
+	})
 }
 
 func (o *OAuth2WechatHandler) Callback(ctx *gin.Context) {
