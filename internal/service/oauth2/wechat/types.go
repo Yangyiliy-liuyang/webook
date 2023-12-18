3dff38af98e648d75f20ecb8233a673b518b77a8
yangyiliy <yangyiliy@foxmail.com>
2023-12-18 22:19:59 +0800

    update

--- internal/service/oauth2/wechat/types.go
+++ internal/service/oauth2/wechat/types.go
@@ -2,50 +2,10 @@ package wechat
 
 import (
 	"context"
-	"fmt"
-	uuid "github.com/lithammer/shortuuid/v4"
-	"net/http"
-	"net/url"
 	"webook/internal/domain"
 )
 
 type Service interface {
 	AuthURL(ctx context.Context) (string, error)
 	VerifyCode(ctx context.Context, code string) (domain.WechatInfo, error)
-}
-
-var redirectURL = url.PathEscape(`https://meoying.com/oauth2/wechat/callback`)
-
-type service struct {
-	appId     string
-	appSecret string
-	client    *http.Client
-}
-
-func NewService(appId string) Service {
-	return &service{appId: appId} //appSecret: appSecret,
-	//client:    http.DefaultClient,
-
-}
-
-// 通过code发起调用获取accessToken
-func (s *service) VerifyCode(ctx context.Context, code string) (domain.WechatInfo, error) {
-	accessTokenUrl := fmt.Sprintf("https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code", s.appId, s.appSecret, code)
-	Req, err := http.NewRequestWithContext(ctx, http.MethodGet, accessTokenUrl, nil)
-	if err != nil {
-		return domain.WechatInfo{}, err
-	}
-	_, err = s.client.Do(Req)
-	if err != nil {
-		return domain.WechatInfo{}, err
-	}
-
-	panic("implement me")
-}
-
-func (s *service) AuthURL(ctx context.Context) (string, error) {
-	//https://developers.weixin.qq.com/doc/oplatform/Website_App/WeChat_Login/Wechat_Login.html
-	const authURLPattern = "https://open.weixin.qq.com/connect/qrconnect?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_login&state=%s#wechat_redirect"
-	state := uuid.New()
-	return fmt.Sprintf(authURLPattern, s.appId, redirectURL, state), nil
 }
