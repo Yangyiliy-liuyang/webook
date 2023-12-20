692c0b34b5b388ea9b9744bf1411899d8e092712
yangyiliy <yangyiliy@foxmail.com>
2023-12-20 22:17:00 +0800

    update

--- internal/service/sms/localsms/service.go
+++ internal/service/sms/localsms/service.go
@@ -11,6 +11,7 @@ type Service struct {
 func NewService() *Service {
 	return &Service{}
 }
+
 func (s *Service) Send(ctx context.Context, tplId string, args []string, phone ...string) error {
 	log.Println(" 验证码: ", args)
 	return nil
