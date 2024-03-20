e9d8ef9125f3294f35f95b5c62deaf98c386bfac
yangyiliy <yangyiliy@foxmail.com>
2024-03-20 19:04:38 +0800

    [update] 缓存创作者列表

---
+++ internal/repository/cache/article.go
@@ -0,0 +1,76 @@
+package cache
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"time"
+	"webook/internal/domain"
+)
+
+type ArticleCache interface {
+	GetFirstPage(ctx context.Context, uid int64) ([]domain.Article, error)
+	SetFirstPage(ctx context.Context, uid int64, articles []domain.Article) error
+	DelFirstPage(ctx context.Context, uid int64) error
+	Get(ctx context.Context, uid int64) (domain.Article, error)
+	Set(ctx context.Context, article domain.Article) error
+}
+
+type ArticleRedisCache struct {
+	cmd redis.Client
+}
+
+func (r *ArticleRedisCache) Set(ctx context.Context, article domain.Article) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r *ArticleRedisCache) Get(ctx context.Context, uid int64) (domain.Article, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r *ArticleRedisCache) DelFirstPage(ctx context.Context, uid int64) error {
+	key := r.firstKey(uid)
+	err := r.cmd.Del(ctx, key).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *ArticleRedisCache) GetFirstPage(ctx context.Context, uid int64) ([]domain.Article, error) {
+	key := r.firstKey(uid)
+	//val, err := r.cmd.Get(ctx, key).Result()
+	val, err := r.cmd.Get(ctx, key).Bytes()
+	if err != nil {
+		return nil, err
+	}
+	var res []domain.Article
+	err = json.Unmarshal([]byte(val), &res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (r *ArticleRedisCache) SetFirstPage(ctx context.Context, uid int64, arts []domain.Article) error {
+	for i := 0; i < len(arts); i++ {
+		arts[i].Content = arts[i].Abstract()
+	}
+	key := r.firstKey(uid)
+	val, err := json.Marshal(arts)
+	if err != nil {
+		return err
+	}
+	err = r.cmd.Set(ctx, key, val, time.Minute*10).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *ArticleRedisCache) firstKey(uid int64) string {
+	return fmt.Sprintf("article:first_page:%d", uid)
+}
